a7841360af56a75a901ad20be88925f63a5c6a9d
DarthDandelion <darthdandelion@galaxy.milkyway.earth>
2013-07-31 14:33:22 -0700

    some simple hook ups

--- QuakeToon/assets/main.cpp
+++ QuakeToon/assets/main.cpp
@@ -1,22 +1,32 @@
 #include <stdio.h>
 
+
 extern "C"
 {
 
-void Qcommon_Init();
-
 void R_BeginRegistration(char *model);
 void DumpLightmapsToPng();
+void Qcommon_Init(int argc, char **argv);
+
+void* Mod_ForName(char *name, bool crash);
+void ExportLevelModel(void *model);
 
 }
 
+
 int main(int argc, char **argv)
 {
-	Qcommon_Init();
+	Qcommon_Init(argc, argv);
 
 	R_BeginRegistration("base1");
 
 	DumpLightmapsToPng();
+
+	void* model = Mod_ForName((char*) "maps/base1.bsp", false);
+
+	if (model)
+		ExportLevelModel(model);
+
 
 	return 0;
 }
---
+++ QuakeToon/assets/q2_export.c
@@ -0,0 +1,10 @@
+
+
+#include "../render/gl_local.h"
+
+void ExportLevelModel(model_t* model)
+{
+
+	printf("Exporting %i surfaces\n", model->numsurfaces);
+
+}
